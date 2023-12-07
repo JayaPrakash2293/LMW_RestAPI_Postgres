6957610c9eee709d9dbc9c87b3617d8d49ee22e9
JayaPrakash2293 <152964083+JayaPrakash2293@users.noreply.github.com>
2023-12-07 17:21:53 +0530

    Correction -2 jp

--- src/test/java/com/example/demo/LmwRestApiPostgresApplicationTests.java
+++ src/test/java/com/example/demo/LmwRestApiPostgresApplicationTests.java
@@ -6,7 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class LmwRestApiPostgresApplicationTests {
 	//Removed Blank line.
-	
 	@Test
 	void contextLoads() {
 	}
